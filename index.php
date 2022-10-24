16f123e0ccd32114d9bd1cfb577d5fd673b38a57
Alexej Mirvoda <77572793+AleMir06@users.noreply.github.com>
2022-10-24 13:32:05 +0200

    Zwischen speicher

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
                     $SRC = "./Bilder3/".$file."";
                     echo "<img src='".$SRC."'/>\n\n";
                 }
+
+            foreach(scandir("./Videos")as $dir) {
+              if($dir === ".." or $dir === ".") continue;
+                echo "<h1>".$dir."<h1>\n";
+                foreach(scandir("./Videos/".$dir.) as $dir2) {
+                  echo "<div class='dropdown'";
+                  echo "\t<button class='dropbtn'>Dropdown<\button>";
+                  echo "\t<div class='dropdown-content'>";
+                  foreach(scandir("./Videos/".$dir."/".$dir2) as $file){
+                    echo "<a href='#'>".$file."</a>";
+                  }
+                }
+            }
         ?>
 
       <div class="dropdown">
