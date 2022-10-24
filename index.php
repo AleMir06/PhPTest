86036f93ead177138d471b4c54a2c18da9ea0e70
Alexej Mirvoda <77572793+AleMir06@users.noreply.github.com>
2022-10-24 09:47:17 +0200

    Dropdown test

--- index.php
+++ index.php
@@ -10,5 +10,15 @@
                     echo "<img src='".$SRC."'/>\n\n";
                 }
         ?>
+
+      <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+
     </body>
 </html>
